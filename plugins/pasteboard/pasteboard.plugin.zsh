61017e5544f422ffd4e1f7ab0f7173492bbda5ed
Chuck D'Antonio <chuck@crdant.io>
2022-05-31 16:20:03 -0400

    Diversified pasteboard plugin

--- plugins/pasteboard/pasteboard.plugin.zsh
+++ plugins/pasteboard/pasteboard.plugin.zsh
@@ -1,8 +1,33 @@
 # Pasteboard Aliases.
 # Use with caution
 #
-alias cdpb='cd $(pbpaste)'           # change to directory on the pasteboard
-alias editpb='${=EDITOR} $(pbpaste)' # edit the file on the pasteboard
-alias openpb='open $(pbpaste)'       # open the url/app/file on the pasteboard
-alias vipb='nvim $(pbpaste)'         # edit the file on the pasteboard with nvim
+alias pbcd='cd $(pbpaste)'           # change to directory on the pasteboard
+alias pbedit='${=EDITOR} $(pbpaste)' # edit the file on the pasteboard
+alias pbopen='open $(pbpaste)'       # open the url/app/file on the pasteboard
+alias pbvi='nvim $(pbpaste)'         # edit the file on the pasteboard with nvim
+alias pbmore='pbpaste | ${PAGER}'    # page through the pastboard content
+alias pbwc='pbpaste | wc'             # count the words on the pasteboard
+alias pbls='ls $(pbpaste)'           # list the directory on the clipboard
+alias pbjq='pbpaste | jq'
+
+function pbcurl () {
+  curl $@ $(pbpaste)
+}
+
 alias pwdpb='pwd | pbcopy'           # put the current working directory on the pasteboard
+
+function catpb () {
+  cat $@ | pbcopy
+}
+
+function lspb () {
+  ls $@ | pbcopy
+}
+
+function jqpb () {
+  jq $@ | pbcopy
+}
+
+function curlpb () {
+  curl $@ | pbcopy
+}
